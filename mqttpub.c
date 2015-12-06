f9f2acc45cfec49db6f1bffe09250b6adcb050f6
mr apa <apa@apa.com>
2015-12-06 20:13:20 +0100

    arguments added

--- mqttpub.c
+++ mqttpub.c
@@ -1,16 +1,12 @@
+#include "unistd.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
 #include "MQTTClient.h"
 
-#define ADDRESS     "tcp://localhost:1883"
 #define CLIENTID    ""
-#define TOPIC       ""
-#define PAYLOAD     ""
 #define QOS         1
 #define TIMEOUT     10000L
-#define USER        ""
-#define PASS        ""
 
 int main(int argc, char* argv[])
 {
@@ -18,13 +14,43 @@ int main(int argc, char* argv[])
   MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
   MQTTClient_message pubmsg = MQTTClient_message_initializer;
   MQTTClient_deliveryToken token;
-  int rc;
+  int rc, opt;
+  char *topic;
+  char *address;
+  
+  /* Parse arguments */
+  if(argc != 11) {
+    fprintf(stderr, "Too few arguments supplied\n");
+    exit(EXIT_FAILURE);
+  }
 
-  conn_opts.username = USER;
-  conn_opts.password = PASS;
+  while((opt = getopt(argc, argv, "t:h:u:p:m:")) != -1) {
+    switch(opt) {
+    case 't':
+      topic = optarg;
+      break;
+    case 'h':         
+      address = optarg;
+      break;
+    case 'u':
+      conn_opts.username = optarg;
+      break;
+    case 'p':
+      conn_opts.password = optarg;
+      break;
+    case 'm':
+      pubmsg.payload = optarg;
+      pubmsg.payloadlen = strlen(optarg);
+      pubmsg.qos = QOS;
+      pubmsg.retained = 0;
+      break;
+    default:
+      fprintf(stderr, "Usage %s -t TOPIC -h ADDRESS -u USER -p PASSWORD -m PAYLOAD\n", argv[0]);
+      exit(EXIT_FAILURE);
+    }
+  }
 
-  MQTTClient_create(&client, ADDRESS, CLIENTID,
-		    MQTTCLIENT_PERSISTENCE_NONE, NULL);
+  MQTTClient_create(&client, address, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
   conn_opts.keepAliveInterval = 20;
   conn_opts.cleansession = 1;
 
@@ -33,14 +59,11 @@ int main(int argc, char* argv[])
       printf("Failed to connect, return code %d\n", rc);
       exit(-1);
     }
-  pubmsg.payload = PAYLOAD;
-  pubmsg.payloadlen = strlen(PAYLOAD);
-  pubmsg.qos = QOS;
-  pubmsg.retained = 0;
-  MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+
+  MQTTClient_publishMessage(client, topic, &pubmsg, &token);
   printf("Waiting for up to %d seconds for publication of %s\n"
 	 "on topic %s for client with ClientID: %s\n",
-	 (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
+	 (int)(TIMEOUT/1000), pubmsg.payload, topic, CLIENTID);
   rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
   printf("Message with delivery token %d delivered\n", token);
   MQTTClient_disconnect(client, 10000);
